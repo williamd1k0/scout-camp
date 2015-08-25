18de1bd87505c3ae48ab192408758d8db95647ca
William Claude Tumeo <tumeowilliam@gmail.com>
2015-08-25 17:13:14 -0300

    Add sql scripts

---
+++ SQL/insert.sql
@@ -0,0 +1,3 @@
+insert into `scout_camp`.`crest` values (null,'Coisa','coisa.jpg',null,null);
+insert into `scout_camp`.`tier` values (null, 'Medalinha','med.jpg',null,null,1);
+insert into `scout_camp`.`user`values (null,null, 'João da Silva',1,1);
