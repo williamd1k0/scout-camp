ae873988ff5ffd0de9e170c87f0435f434797a60
William Claude Tumeo <tumeowilliam@gmail.com>
2015-08-26 11:27:08 -0300

    Add test user inserts

---
+++ SQL/test_user_insert.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: 
+-- Date: 2015-08-26 11:23
+*/
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'João da Silva',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Maria da Silva',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Anna Araujo Gomes',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Cauã Fernandes Cavalcanti',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Enzo Cunha Almeida',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Sofia Goncalves Alves',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Tânia Alves Rodrigues',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Victor Barros Pinto',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Carlos Cardoso Dias',0,0);
+INSERT INTO `scout_camp`.`user` (`user_id`,`facebook_id`,`user_name`,`crest_crest_id`,`tier_tier_id`) VALUES (NULL,NULL,'Luís Sousa Azevedo',0,0);
