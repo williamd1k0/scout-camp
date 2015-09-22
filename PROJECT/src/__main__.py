b40ef4182f8f3f99ffa60f32f2f5e4f6363c000a
William Claude Tumeo <tumeowilliam@gmail.com>
2015-09-22 18:01:49 -0300

    Add base project

---
+++ PROJECT/src/__main__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+"""
+import yaml
+
+teste = file("william.yaml","r")
+json = yaml.load(teste.read())
+teste.close()
+
+print json
+
+teste = file("myth.js","w")
+
+teste.write("coiso")
+teste.write(str(json))
+teste.close()
+"""
+
---
+++ PROJECT/src/scoutcamp/__init__.py
@@ -0,0 +1,1 @@
+from Myth import Myth
