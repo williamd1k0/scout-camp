d42b121bc2c3c0c91bc83ebf063478c8b1f49dcb
William Claude Tumeo <tumeowilliam@gmail.com>
2015-10-03 05:56:35 -0300

    Template tests

--- PROJECT/src/__main__.py
+++ PROJECT/src/__main__.py
@@ -3,3 +3,11 @@
 from scoutcamp import *
 from template import *
 import yaml
+
+if __name__ == '__main__':
+
+    teste = Template("list.yaml","testes/")
+    print teste.get_template_list()
+    print teste.get_templates()
+    print teste.to_string()
+    raw_input()
