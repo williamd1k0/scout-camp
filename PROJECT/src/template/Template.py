af0ff460c82da2c1d5a2ddf4dea906f2bc6e07cb
William Claude Tumeo <tumeowilliam@gmail.com>
2015-10-04 06:04:35 -0300

    Add comentários

--- PROJECT/src/template/Template.py
+++ PROJECT/src/template/Template.py
@@ -31,20 +31,27 @@ class Template:
             Retorno:
               - Sem retorno
         """
+        # Abre o arquivo da lista em YAML
         list_file = open(path+list_file,"r")
+        # Passa o arquivo para uma dict
         template_list = yaml.load(list_file.read())
         list_file.close()
 
+        # Tenta converter a dict para uma list
         self.__set_template_list(template_list)
+        # Se falhar, lança uma exceção do template
         if type(self.__template_list) is not list:
             raise TemplateException("Template list have any typo.")
 
+        # Seta um novo path caso seja diferete do padrão
         if path != self.__path:
             self.__path = path
 
+        # Seta uma nova extensão caso seja diferente do padrão
         if extension != self.__temp_extension:
             self.__temp_extension = extension
 
+        # Seta os templates a partir da lista
         self.__set_templates()
 
 
@@ -60,8 +67,10 @@ class Template:
               - Sem retorno
         """
         template_list = []
+        # Percorre a dict para atribuir a uma list
         for i in temp_dict:
             template_list.append(temp_dict[i])
+        # Atribui à nova lista somente a primeira posição da list
         self.__template_list = template_list[0]
 
 
@@ -87,17 +96,21 @@ class Template:
             Retorno:
               - Sem retorno
         """
+        # Se não foi definido um template, obtém do padrão
         if not path:
             path = self.__path
+        # Se não foi definido uma extensão, obtém do padrão
         if not extension:
             extension = self.__temp_extension
 
         temps = []
+        # Faz um loop pela lista de templates para obtê-los
         for i in self.get_template_list():
             template = open(path+i+extension,"r")
             temps.append(template.read())
             template.close()
 
+        # Seta o resultado no atriburo templates
         self.__templates = temps
 
 
@@ -120,10 +133,11 @@ class Template:
             Retorno:
               - self.__templates (string): templates em uma única string
         """
+        # Junta todas as posições da list em uma string
         return "".join(self.get_templates())
 
 
 if __name__ == '__main__':
-
+    """ Bloco main para testes """
     print dir(Template)
     raw_input()
