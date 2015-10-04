9ece07bf9e7b2e223d37971a8d030cb4c427b68d
William Claude Tumeo <tumeowilliam@gmail.com>
2015-10-04 11:59:41 -0300

    Add build log

--- PROJECT/src/build.bat
+++ PROJECT/src/build.bat
@@ -4,6 +4,10 @@ color e1
 echo.
 echo  Building Scout Camp...
 pyinstaller scout.py
+echo. >> build-log.txt
+echo  -- Build done -- >> build-log.txt
+echo  %date% >> build-log.txt
+echo  %time% >> build-log.txt
 echo.
 echo  Build Done.
 pause >nul
