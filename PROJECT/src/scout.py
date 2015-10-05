ced20030375e71fd11f689b1026cfdf69e59dd0e
William Claude Tumeo <tumeowilliam@gmail.com>
2015-10-04 23:18:13 -0300

    Add argsparser

--- PROJECT/src/scout.py
+++ PROJECT/src/scout.py
@@ -2,6 +2,7 @@
 
 from scoutcamp import *
 from template import *
+import argparse
 import yaml
 
 class ScoutCamp:
@@ -14,13 +15,30 @@ class ScoutCamp:
 
         print teste.get_template_list()
         print teste.get_templates()
-        print teste.to_string()
+        print teste
 
     @staticmethod
     def myth():
         print "== Penso, logo mito =="
 
+__version__ = "Scout Camp 0.0.1"
+
 if __name__ == '__main__':
 
-    ScoutCamp.myth()
-    raw_input()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--debug", help="init the debug mode.", action="store_true")
+    parser.add_argument("-m","--myth", help="myth.", action="store_true")
+    parser.add_argument("-v","--version", help="show version.", action="store_true")
+    args = parser.parse_args()
+
+    if args.myth:
+        ScoutCamp.myth()
+
+    elif args.debug:
+        ScoutCamp.main()
+
+    elif args.version:
+        print __version__
+
+    else:
+        pass
