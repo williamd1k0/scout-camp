033531784a9970079fd201b38caa457659bb9495
William Claude Tumeo <tumeowilliam@gmail.com>
2015-10-05 18:49:56 -0300

    Add debug mode

--- PROJECT/src/scout.py
+++ PROJECT/src/scout.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
 from scoutcamp import *
 from template import *
-import argparse
 import yaml
 
 class ScoutCamp:
@@ -11,12 +11,19 @@ class ScoutCamp:
 
 
     @staticmethod
-    def main(**debug):
-        if not debug:
-            debug["path"] = ""
+    def main(debug_path=None, confoverride=None):
+        if not debug_path:
+            debug_path = ""
+        if confoverride:
+            configs = Config(confoverride)
+        else:
+            configs = Config()
 
-        configs = Config()
-        teste = Template("list.yaml", debug["path"]+configs.get_path_to("templates"))
+        try:
+            teste = Template(configs.get_list_to("templates"),
+                             debug_path+configs.get_path_to("templates"))
+        except IOError as ioe:
+            TemplateException("list.yaml for templates not found")
 
         print teste.get_template_list()
         print teste.get_templates()
@@ -25,7 +32,10 @@ class ScoutCamp:
 
     @classmethod
     def debug(cls, path="testes/"):
-        cls.main(**{"path":path})
+        path = path.replace("\\","/")
+        if path[-1] != "/":
+            path += "/"
+        cls.main(debug_path=path)
 
 
     @staticmethod
@@ -40,18 +50,28 @@ class ScoutCamp:
 
 
 if __name__ == '__main__':
-    #print help(argparse.ArgumentParser)
+
+    import argparse
+
     parser = argparse.ArgumentParser(prog="Scout Camp", description="Scout Camp - Static HTML Group Manager")
     parser.add_argument("-d","--debug", help="run the debug mode")
+    parser.add_argument("-c","--conf", help="use another config file")
     parser.add_argument("-m","--myth", help="myth", action="store_true")
     parser.add_argument("-v","--version", help="show version", action="store_true")
+    parser.add_argument("-q","--qqqq", help="coiso da Gabi", action="store_true")
     args = parser.parse_args()
 
     if args.myth:
         ScoutCamp.myth()
 
     elif args.debug:
-        ScoutCamp.debug()
+        ScoutCamp.debug(args.debug)
+
+    elif args.conf:
+        ScoutCamp.main(confoverride=args.conf)
+
+    elif args.qqqq:
+        print "Enfim . . ."
 
     elif args.version:
         print ScoutCamp.get("version")
