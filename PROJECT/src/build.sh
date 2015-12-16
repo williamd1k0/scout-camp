2d6933af19cebedcc7f6b06c82a8f0cf77efb61b
William Claude Tumeo <tumeowilliam@gmail.com>
2015-12-16 18:37:42 -0200

    Remove project files

--- PROJECT/src/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-path_src='PROJECT/src'
-path_proj='PROJECT/base_project'
-echo 'ScoutCamp Linux Build (w/ render)'
-python "$path_src/scout.py" --version
-python "$path_src/scout.py" --path "$path_proj" --render
--- PROJECT/src/scoutcamp/__init__.py
+++
@@ -1,6 +0,0 @@
-from config import Config
-from database import *
-from server import Server
-from lang import Lang
-from template import Template
-from exceptions import *
