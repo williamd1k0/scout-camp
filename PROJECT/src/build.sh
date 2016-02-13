3eaa0f5f0d197432488fe3a294f3b0f88d3e456c
William Claude Tumeo <tumeowilliam@gmail.com>
2016-02-13 02:07:25 -0200

    Teste usando pip requirements

--- PROJECT/src/build.sh
+++ PROJECT/src/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 path_src='PROJECT/src'
 path_proj='PROJECT/base_project'
+pip install -r "$path_src/requirements.txt"
 echo 'ScoutCamp Linux Build (w/ render)'
 echo '$ scout --version'
 python "$path_src/scout.py" --version
