0219523697b58ff9904b63acb28974f1c07bbeea
William Claude Tumeo <tumeowilliam@gmail.com>
2016-02-14 19:00:31 -0200

    Add packed version

--- PROJECT/src/build.bat
+++ PROJECT/src/build.bat
@@ -2,8 +2,22 @@
 title  Scout Camp Builder
 color e1
 echo.
-echo  Building Scout Camp...
+echo  Checking Python
+python --version
+echo.
+echo  Checking PyInstaller...
+pip install pyinstaller
+pyinstaller -v
+echo.
+echo  Building ScoutCamp...
+echo.
+python scout.py -v
+echo.
+echo  Unpacked version...
 pyinstaller -i py.ico scout.py
+echo.
+echo  Packed version...
+pyinstaller -F -i py.ico scout.py
 echo. >> build-log.txt
 echo  -- Build done -- >> build-log.txt
 echo  %date% >> build-log.txt
