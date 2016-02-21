f0f01e82e1ca1a81ab148fec7e2133fc71a33ffd
William Claude Tumeo <tumeowilliam@gmail.com>
2016-02-20 22:20:54 -0200

    Fix travis-ci

--- PROJECT/src/build.sh
+++ PROJECT/src/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 path_src='PROJECT/src'
-path_proj='PROJECT/base_project'
 pip install -r "$path_src/requirements.txt"
 echo 'ScoutCamp Linux Build (w/ render)'
 echo '$ scout --version'
 python "$path_src/scout.py" --version
-echo '$ scout --path "$path_proj" --render'
-python "$path_src/scout.py" --path "$path_proj" --render
+echo "$path_src/scout.py --create base_project"
+python "$path_src/scout.py" --create "base_project"
+echo '$ scout --path base_project --render'
+python "$path_src/scout.py" --path "base_project" --render
