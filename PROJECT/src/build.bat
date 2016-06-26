009dedb0a165852cf922a1d2bc12df1356bb7502
William Tumeo <tumeowilliam@gmail.com>
2016-06-25 21:39:53 -0300

    Build check

--- PROJECT/src/build.bat
+++ PROJECT/src/build.bat
@@ -2,17 +2,17 @@
 title  ScoutCamp Builder
 echo.
 echo  Checking Python
-python --version
+py -3 --version
 echo.
 echo  Checking PyInstaller...
-pyinstaller -v
+pyinstaller3 -v
 echo.
 echo  Building ScoutCamp...
 echo.
 python main.py -v
 echo.
 echo  Packed version...
-pyinstaller -F -i scicon.ico -n camp main.py
+pyinstaller3 -F -i scicon.ico -n camp main.py
 echo. >> build-log.txt
 echo  -- Build done -- >> build-log.txt
 echo  %date% >> build-log.txt
